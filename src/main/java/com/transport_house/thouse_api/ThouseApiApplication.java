8fa825a48b91320f652481a9d9e2e67163d69521
lucianoheric <lucianoheric55@gmail.com>
2026-01-10 23:48:42 -0300

    first commit

---
+++ src/main/java/com/transport_house/thouse_api/ThouseApiApplication.java
@@ -0,0 +1,13 @@
+package com.transport_house.thouse_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThouseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThouseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/transport_house/thouse_api/ThouseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.transport_house.thouse_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThouseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
